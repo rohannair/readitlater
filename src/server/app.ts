4a5b364ae350d67865a499c29380b62a0e329d95
Rohan Nair <rn@rohan.ai>
2024-06-25 17:23:39 -0400

    convert url route to openapi enabled

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,3 +1,4 @@
+import { env } from '@/env'
 import type { Env } from '@/server/types'
 import { OpenAPIHono as Hono } from '@hono/zod-openapi'
 import { apiReference } from '@scalar/hono-api-reference'
@@ -5,24 +6,25 @@ import { showRoutes } from 'hono/dev'
 import { logger } from 'hono/logger'
 import { authRouter } from './auth'
 import { apiRouter } from './scraper'
-import { env } from '@/env'
 
 export const app = new Hono<Env>()
-  // .basePath('/api')
   .use(logger())
-  .get('/', (c) => c.text('Hello World!'))
+  .get('/', (c) =>
+    c.json({ message: 'Healthy', uptime: process.uptime() }, 200),
+  )
   .get('/health', (c) =>
-    c.json({ message: 'Healthy', uptime: process.uptime() }),
+    c.json({ message: 'Healthy', uptime: process.uptime() }, 200),
   )
 
 // @ts-expect-error
 app.doc('/api/swagger.json', {
   openapi: '3.1.0',
-  info: { title: 'Read it later', version: '1.0.0' },
+  info: { title: 'Read it later API', version: '1.0.0' },
+  servers: [{ url: 'http://localhost:3000/api' }],
 })
 
 app.get(
-  '/api/scalar',
+  '/api/docs',
   apiReference({
     spec: {
       url: '/api/swagger.json',
@@ -30,7 +32,7 @@ app.get(
   }),
 )
 
-const routes = app.route('/auth', authRouter).route('/v1', apiRouter)
+const routes = app.route('/auth', authRouter).route('/', apiRouter)
 
 // for RPC
 export type AppType = typeof routes
--- src/server/scraper/routes.ts
+++ src/server/scraper/routes.ts
@@ -1,36 +1,11 @@
 import { verifySession } from '@/server/_middleware/verifySession'
-import { linkRepository } from '@/server/db/repositories/links.repository'
-import { queueScrape } from '@/server/scraper/queueScrape'
 import type { Env } from '@/server/types'
 import { OpenAPIHono as Hono } from '@hono/zod-openapi'
-import { zValidator } from '@hono/zod-validator'
-import { z } from 'zod'
-
-const urlSchema = z.object({
-  url: z.string().url(),
-})
+import { queueScrape } from './handlers'
 
 export const apiRouter = new Hono<Env>()
   .use(verifySession)
-  .get('/url', zValidator('query', urlSchema), async (c) => {
-    const { url } = c.req.valid('query')
-
-    if (!c.var.user?.id) {
-      return c.json({ message: 'Not logged in' }, 401)
-    }
-
-    const link = await linkRepository.createLink({
-      url,
-      userId: c.var.user?.id,
-    })
-
-    if (!link?.id) {
-      return c.json({ message: 'Link already exists' }, 400)
-    }
-
-    const { message } = await queueScrape({ url, link: link.id })
-    return c.json({ message })
-  })
+  .route('/', queueScrape)
   .get('/url/status', async (c) => {
     return c.json({ message: 'Status' })
   })
