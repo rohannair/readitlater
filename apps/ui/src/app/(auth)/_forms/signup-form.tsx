1a32b21351cb1753ecbebf9bd16229c9c80ea438
Rohan Nair <rn@rohan.ai>
2024-07-29 15:19:55 -0400

    update signup form to remove conform

--- apps/ui/src/app/(auth)/_forms/signup-form.tsx
+++ apps/ui/src/app/(auth)/_forms/signup-form.tsx
@@ -1,13 +1,25 @@
 'use client'
 
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
 import { FormInput } from '@/app/(auth)/_forms/form-input'
 import { Button } from '@/components/ui/button'
 import { signup } from '@/actions/signup'
-import { useForm } from '@conform-to/react'
+import { useForm } from 'react-hook-form'
 import { parseWithZod } from '@conform-to/zod'
 import { redirect } from 'next/navigation'
 import { useFormState } from 'react-dom'
 import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Input } from '@/components/ui/input'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 const MIN_PASSWORD_LENGTH = 5
 const createUserSchema = z.object({
@@ -28,73 +40,79 @@ const createUserSchema = z.object({
   redirect: z.string().default('/'),
 })
 
-const parseData = (formData: FormData) => {
-  return parseWithZod(formData, { schema: createUserSchema })
-}
-
-async function submitCreateUser(_prevState: unknown, formData: FormData) {
-  const result = parseData(formData)
-
-  if (result.status !== 'success') {
-    return result.reply()
-  }
-
-  const { redirect: redirectUrl, ...value } = result.value
-
-  await signup(value)
-
-  redirect(redirectUrl)
-}
-
 export function SignupForm({
   redirect,
 }: {
   redirect: string
 }) {
-  const [lastResult, action] = useFormState(submitCreateUser, undefined)
-  const [form, fields] = useForm({
-    // Sync the result of last submission
-    lastResult,
-
-    // Reuse the validation logic on the client
-    onValidate({ formData }) {
-      return parseData(formData)
+  const router = useRouter()
+  const [error, setError] = useState('')
+  const form = useForm<z.infer<typeof createUserSchema>>({
+    resolver: zodResolver(createUserSchema),
+    defaultValues: {
+      email: '',
+      password: '',
     },
-
-    // Validate the form on blur event triggered
-    shouldValidate: 'onBlur',
-    shouldRevalidate: 'onInput',
   })
 
+  const submitRegister = async (values: z.infer<typeof createUserSchema>) => {
+    try {
+      const res = await signup(values)
+
+      // @ts-expect-error: RPC breakdown
+      if (res?.user) {
+        router.push(redirect)
+      }
+    } catch (_) {
+      setError('Invalid email or password')
+    }
+  }
+
   return (
-    <form id={form.id} onSubmit={form.onSubmit} action={action} noValidate>
-      <div className="grid w-full items-center gap-4">
-        <FormInput
-          id="email"
-          // key={fields.email.key}
-          name={fields.email.name}
-          type="email"
-          placeholder="hello@example.com"
-          errors={fields.email.errors}
-          autoFocus
-        >
-          Email
-        </FormInput>
-        <FormInput
-          id="password"
-          // key={fields.password.key}
-          name={fields.password.name}
-          type="password"
-          errors={fields.password.errors}
-        >
-          Password
-        </FormInput>
-        <input type="hidden" name="redirect" value={redirect} />
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(submitRegister)}
+        className="grid w-full items-center gap-4"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="hello@example.com" autoFocus />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input {...field} type="password" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {error ? (
+          <div className="bg-red-100 text-red-800 p-2 text-sm rounded-sm">
+            <span className="font-bold">Error: </span>
+            {error}
+          </div>
+        ) : null}
 
         <div className="flex justify-between">
-          <Button type="submit">Signup</Button>
+          <Button type="submit">Register</Button>
         </div>
-      </div>
-    </form>
+      </form>
+    </Form>
   )
 }
